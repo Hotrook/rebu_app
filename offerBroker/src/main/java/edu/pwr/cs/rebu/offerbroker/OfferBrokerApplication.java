98a8a9c6fb7ca48d5418d7aa6f4a16b18e169f3c
Marcin Regulski <mregulski@spyro-soft.com>
2018-12-03 16:29:10 +0100

    initial backend setup

---
+++ offerBroker/src/main/java/edu/pwr/cs/rebu/offerbroker/OfferBrokerApplication.java
@@ -0,0 +1,12 @@
+package edu.pwr.cs.rebu.offerbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferBrokerApplication.class, args);
+	}
+}
